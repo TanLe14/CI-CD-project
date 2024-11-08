1d245a21a884aabe580d285f6c2dd810ee823942
TanLe14 <104408349+TanLe14@users.noreply.github.com>
2024-11-08 14:37:01 +0700

    Update app.py

--- flask-sklearn/app.py
+++ flask-sklearn/app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h2>Sklearn Prediction Home APP - RestAPI TanLX </h2>"
+    html = "<h2>Sklearn Prediction Home APP - RestAPI </h2>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
